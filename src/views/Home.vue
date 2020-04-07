e8d92288e0892bee70580fd8f0b6d74d69b7b3c2
Viktor <enotcode@gmail.com>
2020-04-07 10:41:22 +0300

    added requests to api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,36 @@
                         SHORT
                     </v-btn>
               </template>
-              </v-text-field>          
+              </v-text-field> 
           </v-flex>
+          <a :href="short_url" target="_blank">{{ short_url }}</a>
       </v-layout>
   </div>
 </template>
+
+<script>
+  import axios from "axios"
+
+  export default{
+    data(){
+      return{
+        url: "",
+        short_url: ""
+      }
+    },
+    methods: {
+        short(){
+            axios.post("https://myshortyapi.herokuapp.com/", {
+                url: this.url
+            })
+            .then(response => {
+                this.short_url += "https://myshortyapi.herokuapp.com/u/" + response.data.token;
+                console.log(response.data.token);
+            })
+            .catch(error => {
+                console.log(error);
+            });        
+        }
+    }
+  }
+</script>
