0f68f7851d70ecbd2d088a39e7d77b0fa45dffe6
Viktor <enotcode@gmail.com>
2020-04-07 11:18:25 +0300

    card component

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    class="mx-auto"
+    width="40em"
+  >
+    <v-card-text>
+      <p class="title">{{ title }}</p>
+      <a class="subtitle-1" :href="short_url" target="_blank">{{ short_url }}</a>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    short_url: String
+  }
+};
+</script>
