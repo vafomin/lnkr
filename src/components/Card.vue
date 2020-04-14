f27d908b1ed38d35295edf465453bae36e5ad9cc
Viktor <enotcode@gmail.com>
2020-04-14 19:11:02 +0300

    add QR-code

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,6 +6,25 @@
     <v-card-text>
       <p class="title">{{ title }}</p>
       <a class="subtitle-1" :href="short_url" target="_blank">{{ short_url }}</a>
+      <v-spacer></v-spacer>
+      <v-btn
+        color="primary"
+        outlined
+        @click.stop="dialog = true"
+      >
+        QR-code
+      </v-btn>
+      <v-dialog
+        v-model="dialog"
+        max-width="290"
+      >
+      <v-card>
+        <v-card-title class="headline">QR-code</v-card-title>
+        <v-card-text>
+          <qrcode :value="title" :options="{ width: 200 }"></qrcode>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
     </v-card-text>
   </v-card>
 </template>
@@ -16,6 +35,11 @@ export default {
   props: {
     title: String,
     short_url: String
+  },
+  data () {
+    return {
+      dialog: false,
+    }
   }
 };
 </script>
