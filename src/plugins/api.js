88e0ff28d3349a55deb2d595b9e2fcfd8694fcb6
Viktor <enotcode@gmail.com>
2020-04-21 13:44:48 +0300

    added getWatch

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,30 +1,37 @@
-import axios from "axios"
+import axios from 'axios'
 
-axios.defaults.baseURL = "https://api.shrt.xyz";
+axios.defaults.baseURL = "https://api.shrt.xyz/";
 
 const api = {
-    async getToken(token){
-        return await axios.get(`/get?token=${token}`)
-        .then(response => {
-            return response.data;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-
-    async shortURL(myURL){
+    async shortURL(myURL) {
         return await axios.post("/short", {
             url: myURL
         })
-        .then(response => {
-            return response.data.token;
-        })
-        .catch(error => {
-            console.log(error);
-        });
+            .then(response => {
+                return response.data.token;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    async getToken(token) {
+        return await axios.get(`/get?token=${token}`)
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    async getWatch(token) {
+        return await axios.get(`/getWatch?token=${token}`)
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-
-}
+};
 
 export default api;
