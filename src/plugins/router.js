6eab66f5c1e8d372121630b15231782d0f3eb9fe
Viktor <enotcode@gmail.com>
2020-04-21 13:45:58 +0300

    code style

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,30 +1,30 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "../views/Home";
-import Page from "../views/Page";
-import NotFound from "../views/NotFound";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '../views/Home';
+import Page from '../views/Page';
+import NotFound from '../views/NotFound';
 
 Vue.use(Router);
 
 const router = new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/u/:token",
-      name: "page",
-      component: Page
-    },
-    {
-      path: "*",
-      name: "notFound",
-      component: NotFound
-    }
-  ]
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/u/:token",
+            name: "page",
+            component: Page
+        },
+        {
+            path: "*",
+            name: "notFound",
+            component: NotFound
+        }
+    ]
 });
 
 export default router;
