2f71791032f1a7790a0fb3a16f19c897412d349b
Viktor Fomin <enotcode@gmail.com>
2021-03-13 00:02:15 +0300

    Initial commit

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  app: {
+    name: "Shorty",
+    version: "1.3.0",
+    url: "http://localhost:8081",
+    serverURL: "http://localhost:4000",
+  },
+};
---
+++ frontend/src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import router from "./plugins/router";
+import vuetify from "./plugins/vuetify";
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+Vue.component(VueQrcode.name, VueQrcode);
+
+new Vue({
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
