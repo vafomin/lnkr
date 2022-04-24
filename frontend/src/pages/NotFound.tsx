c8eb775f867729551f5ecc52dd6bb4cf848f7c4a
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-04-24 07:02:09 +0300

    frontend: feat: add not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,21 @@
+import { Box, Heading, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 const NotFound: React.FC = () => {
-  return <h1>Not found</h1>;
+  let navigate = useNavigate();
+
+  const onBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" pt={8} alignItems="center">
+      <Heading mb={4}>There doesn't seem to be anything here...</Heading>
+      <Button size="lg" onClick={onBack}>
+        Back to home
+      </Button>
+    </Box>
+  );
 };
 
 export default NotFound;
