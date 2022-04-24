77b970aaf3309b5279c960971dee7a338c19c3be
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-04-24 07:21:22 +0300

    frontend: feat: clipboard copy

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,5 +1,7 @@
-import { Box, Text, Link, Button } from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { useCallback } from "react";
+import { Box, Text, Link, Button, useToast } from "@chakra-ui/react";
+import { ExternalLinkIcon, CopyIcon } from "@chakra-ui/icons";
+import { useClipboard } from "use-clipboard-copy";
 
 interface CardProps {
   url: string;
@@ -8,6 +10,19 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = (props) => {
   const { url, token } = props;
+  const clipboard = useClipboard();
+  const toast = useToast();
+
+  const onCopyClick = useCallback(() => {
+    clipboard.copy(url);
+    toast({
+      position: "top-right",
+      title: "Copied!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    });
+  }, [clipboard.copy]);
 
   return (
     <Box w="100%" p={4} borderRadius="lg" borderWidth="1px">
@@ -20,7 +35,12 @@ const Card: React.FC<CardProps> = (props) => {
         </Link>
       </Box>
       <Box display="flex" gap={2} pt={4}>
-        <Button size="sm" colorScheme="green">
+        <Button
+          size="sm"
+          colorScheme="green"
+          leftIcon={<CopyIcon />}
+          onClick={onCopyClick}
+        >
           Copy link
         </Button>
         <Button size="sm" colorScheme="teal">
